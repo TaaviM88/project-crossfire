5dc23fb6d03e0ed6b1ffe86b1cec5f69d0727d0d
AKSELI-LAPTOP\Akseli <azzeli91@gmail.com>
2018-09-26 12:40:18 +0300

    Liikkumista muutettu, sekä pyöriminen lisätty (wip)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     public float speed;
-
+    public float rotate = 1;
     private Rigidbody rb;
 
     private void Start()
@@ -14,14 +14,37 @@ public class PlayerMovement : MonoBehaviour {
     }
 
     // Use this for initialization
+    void Update()
+    {
+        //transform.Rotate(new Vector3(0.0f,1,0.0f), rotate * Time.deltaTime);
+    }
+
     void FixedUpdate () {
 
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+        
+        //float rotateHorizontal = Input.GetAxis("Horizontal") * rotate * Time.deltaTime;
+        //float rotateVertical = Input.GetAxis("Vertical") * rotate * Time.deltaTime;
 
-        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical) * speed * Time.deltaTime;
+        
 
-        rb.MovePosition(transform.position + movement);
+        Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            rotate = 20;
+
+        }
+
+        Debug.Log("ö" + rb.velocity.magnitude);
+
+        rb.AddTorque(new Vector3(0, rb.velocity.magnitude * rotate, 0));
+        //rb.AddTorque(new Vector3(0, rotateVertical, 0));
+
+        //rb.MovePosition(transform.position + movement);
 
 		
 	}
