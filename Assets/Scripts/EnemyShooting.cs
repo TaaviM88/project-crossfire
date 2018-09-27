e5b586fb6f07554444239bb0ef38b1fdeaaa03ed
TaaviM88Laptop <juomakuski@hotmail.com>
2018-09-27 11:02:47 +0300

    Player prefab location changed to Prefab folder. Enemy prefab created.

---
+++ Assets/Scripts/EnemyShooting.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour {
+    public Transform player;
+    Vector3 lookVector;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        lookVector = player.transform.position - transform.position;
+        lookVector.y = transform.position.y;
+        Quaternion rot = Quaternion.LookRotation(lookVector);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rot, 1);
+	}
+}
