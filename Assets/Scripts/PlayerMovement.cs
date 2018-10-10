9189ec569392b52145a9c9c8675e33766d82c8c5
AKSELI-LAPTOP\Akseli <azzeli91@gmail.com>
2018-10-10 12:04:52 +0300

    Added new dash script to the PlayerMovement script.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+    private Rigidbody rb;
+
+    public float baseSpeed;
     public float speed;
     public float dash;
     public float rotate = 1;
-    private Rigidbody rb;
-    
+
+    public float dashTime;
+    private float currentDashTime = 0f;
+    public float dashDelay;
+    public float currentDashDelayTime = 0f;
+    public float time;
+    public bool hasDashed = false;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        currentDashTime = 0f;
+        currentDashDelayTime = 0f;
+        speed = baseSpeed;
     }
 
     void FixedUpdate () {
@@ -24,17 +36,34 @@ public class PlayerMovement : MonoBehaviour {
 
         rb.AddForce(movement * speed);
 
-        if (Input.GetButton("Fire1"))
-        {
-            rb.AddForce(movement * dash, ForceMode.Impulse);  
-        }
-
-        if (rb.velocity.magnitude > speed)
+        if (rb.velocity.magnitude > speed) 
         {
             rb.velocity = rb.velocity.normalized * speed;
         }
         
-        rb.AddTorque(new Vector3(0, rb.velocity.magnitude * rotate, 0));
+        rb.AddTorque(new Vector3(0, rb.velocity.magnitude * rotate, 0)); //Adds y-axis rotation
 
-	}
+        if (Input.GetButton("Fire1") && !hasDashed && Time.time > currentDashDelayTime)
+        {
+            currentDashTime = Time.time + dashTime; //Starting the timer for dash
+            hasDashed = true;
+            //rb.AddForce(movement * dash, ForceMode.Impulse);  
+        }
+
+        if ((Time.time > currentDashTime) && hasDashed) //Am I dashing or has the dash timer expired?
+        {
+            hasDashed = false;
+            currentDashDelayTime = Time.time + dashDelay;
+        }
+
+        if (hasDashed)
+        {
+            speed = dash;
+        }
+
+        if (!hasDashed)
+        {
+            speed = baseSpeed;
+        }
+    }
 }
