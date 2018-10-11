1da770de7ec01ea158a05df0d88efa1f2caea98b
TaaviM88Laptop <juomakuski@hotmail.com>
2018-10-11 09:52:16 +0300

    MainMenu play button created

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+    public int  LevelIndex = 1;
+        
+    public void LoadGameScene()
+    {
+        //ladataan kenttä jonka index on build setting numerossa 1; Tässä tapauksessa Level1.
+        Debug.Log("Ladataan Scene");
+        SceneManager.LoadScene(1);
+    }
+}
